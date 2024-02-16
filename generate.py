9aea0f4dfba89706d3f470ae493a0e622430ee5b
Bahy <35463814+bahyali@users.noreply.github.com>
2024-02-16 22:20:04 +0000

    fix duplicates and use chat model

--- generate.py
+++ generate.py
@@ -1,7 +1,7 @@
 import csv
 from openai import OpenAI
 
-client = OpenAI(api_key='api-key')
+client = OpenAI(api_key='ur-api-key')
 
 # Replace 'your_api_key_here' with your actual OpenAI API key
 
@@ -13,25 +13,34 @@ EMOTIONS = [
     "Anger",
     "Surprise",
     "Contempt",
-    "Embarrassment"]
+    "Embarrassment"
+]
 NUM_UTTERANCES_PER_EMOTION = 10
 
 
 def generate_utterances_for_emotion(emotion, num_utterances=150):
-    utterances = []
-    for _ in range(num_utterances):
+    utterances = set()
+    while len(utterances) < num_utterances:
         try:
-            response = client.completions.create(model="gpt-3.5-turbo-0125",
-                                                 prompt=f"Generate an utterance that expresses {emotion.lower()}:",
-                                                 max_tokens=60,
-                                                 n=1,
-                                                 stop=None,
-                                                 temperature=0.7)
-            utterances.append(response.choices[0].text.strip())
+            response = client.chat.completions.create(
+                model="gpt-3.5-turbo-0125",
+                messages=[
+                    {
+                        "role": "system",
+                        "content": "Act as a cutomer in a customer service conversation."},
+                    {
+                        "role": "user",
+                        "content": f"Generate an utterance that expresses {emotion.lower()}:",
+                    }],
+                max_tokens=60,
+                n=1,
+                stop=None,
+                temperature=0.7)
+            utterances.add(response.choices[0].message.content.strip())
         except Exception as e:
             print(f"An error occurred: {e}")
             break
-    return utterances
+    return list(utterances)
 
 
 def main():
